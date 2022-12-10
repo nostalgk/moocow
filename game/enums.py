2872ff02a0f3b7d73652b44996c7e767e91a6051
nostalgk <totallynotkylelunde@gmail.com>
2022-12-09 18:54:04 -0600

    Added basic object function and test (debugged lol, from beginner tutorial)

---
+++ game/enums.py
@@ -0,0 +1,24 @@
+
+class Ability(Enum):
+    """
+    The six base ability-bonuses and other 
+    abilities
+
+    """
+
+    STR = "strength"
+    DEX = "dexterity"
+    END = "endurance"
+    INT = "intelligence"
+    PER = "perception"
+    WIL = "willpower"
+     
+    ARMOR = "armor"
+    WEAPON = "weapon"
+    
+    CRITICAL_FAILURE = "critical_failure"
+    CRITICAL_SUCCESS = "critical_success"
+    
+    ALLEGIANCE_HOSTILE = "hostile"
+    ALLEGIANCE_NEUTRAL = "neutral"
+    ALLEGIANCE_FRIENDLY = "friendly"
